43074ab44075e0ff16799c76aa160fc8f20fbb4e
1298743328szyasy <1298743328@qq.com>
2019-06-16 23:52:35 +0800

    SpringMVC的使用

---
+++ src/main/java/cn/imnu/handler/MyHandler.java
@@ -0,0 +1,19 @@
+package cn.imnu.handler;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyHandler implements Controller {
+
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        //装载模型数据和逻辑视图
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name","Tom");
+        //添加逻辑视图
+        modelAndView.setViewName("show");
+        return modelAndView;
+    }
+}
